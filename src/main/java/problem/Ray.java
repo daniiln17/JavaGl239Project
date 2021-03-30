bd58a81adc5dfd14b75ab1a83f99ff4748346332
nigmatulindk.22 <daniiln17@mail.ru>
2021-03-30 15:05:49 +0300

    rer

--- src/main/java/problem/Ray.java
+++ src/main/java/problem/Ray.java
@@ -12,7 +12,7 @@ public class Ray {
 
     public Vector2 o;
 
-    public double d;
+    //public double d;
 
     public Ray(Vector2 a, Vector2 b) {
         A = a;
@@ -33,19 +33,14 @@ public class Ray {
         ));
     }
 
-    public void find(Vector2 a, Vector2 b, Vector2 c, Vector2 d, Vector2 pos) {
-        A = a;
-        B = b;
-        C = c;
-        D = d;
-
+    public double find(Circle circle) {
         double k = B.minus(A).len();
         double l = B.minus(C).len();
         double S1 = l * k;//площадь широкого луча
-        double sa = ((pos.x - a.x) * (b.y - a.y) - (b.x - a.x) * (pos.y - a.y)) / 2;
-        double sb = ((pos.x - c.x) * (b.y - c.y) - (b.x - c.x) * (pos.y - c.y)) / 2;
-        double sc = ((pos.x - c.x) * (d.y - c.y) - (d.x - c.x) * (pos.y - c.y)) / 2;
-        double sd = ((pos.x - a.x) * (b.y - a.y) - (b.x - a.x) * (pos.y - a.y)) / 2;
+        double sa = ((circle.pos.x - A.x) * (B.y - A.y) - (B.x - A.x) * (circle.pos.y - A.y)) / 2;
+        double sb = ((circle.pos.x - C.x) * (B.y - C.y) - (B.x - C.x) * (circle.pos.y - C.y)) / 2;
+        double sc = ((circle.pos.x - C.x) * (D.y - C.y) - (D.x -C.x) * (circle.pos.y - C.y)) / 2;
+        double sd = ((circle.pos.x - A.x) * (B.y - A.y) - (B.x - A.x) * (circle.pos.y - A.y)) / 2;
         double S2 = sa + sb + sc + sd;// сумма площадей треугольников с вершинами в центре луча и в центре окружности
         if (S1 > S2) {
             System.out.print("за");
@@ -53,10 +48,51 @@ public class Ray {
         if (S1 < S2) {
             System.out.print("внутри");
         }
-        Line l1 = new Line(a.x, a.y, d.x, d.y);
+        Line l1 = new Line(A.x, A.y, D.x, D.y);
+        double a1 = A.y - D.y;
+        double b1 = D.x - A.x;
+        double c1 = A.x * D.y - D.x * A.y;
+        double d1 = Math.pow((b1 * c1 + a1 * b1 * circle.pos.x - a1 * a1 * circle.pos.y), 2) - (a1 * a1 + b1 * b1) * (c1 * c1 + 2 * a1 * c1 - circle.pos.x + a1 * a1 * (circle.pos.x * circle.pos.x + circle.pos.y * circle.pos.y - circle.r * circle.r));
+        if(d1>0){
+            System.out.println("две общие точки");
+        }
+        if(d1==0){
+            System.out.println("касание");
+        }
+        if(d1<0){
+            System.out.println("нет");
+        }
+
         Line l2 = new Line(a.x, a.y, b.x, b.y);
+        double a2 = a.y - b.y;
+        double b2 = b.x - a.x;
+        double c2 = a.x * b.y - b.x * a.y;
+        double d2 = Math.pow((b2 * c2 + a2 * b2 * circle.pos.x - a2 * a2 * circle.pos.y), 2) - (a2 * a2 + b2 * b2) * (c2 * c2 + 2 * a2 * c2 - circle.pos.x + a2 * a2 * (circle.pos.x * circle.pos.x + circle.pos.y * circle.pos.y - circle.r * circle.r));
+        if(d2>0){
+            System.out.println("две общие точки");
+        }
+        if(d2==0){
+            System.out.println("касание");
+        }
+        if(d2<0){
+            System.out.println("нет");
+        }
+
         Line l3 = new Line(b.x, b.y, c.x, c.y);
-        Line l4 = new Line(c.x, c.y, d.x, d.y);
+        double a3 = b.y - c.y;
+        double b3 = c.x - b.x;
+        double c3 = b.x * c.y - c.x * b.y;
+        double d3 = Math.pow((b3 * c3 + a3 * b3 * circle.pos.x - a3 * a3 * circle.pos.y), 2) - (a3 * a3 + b3 * b3) * (c3 * c3 + 2 * a3 * c3 - circle.pos.x + a3 * a3 * (circle.pos.x * circle.pos.x + circle.pos.y * circle.pos.y - circle.r * circle.r));
+        if(d3>0){
+            System.out.println("две общие точки");
+        }
+        if(d3==0){
+            System.out.println("касание");
+        }
+        if(d3<0){
+            System.out.println("нет");
+        }
+
 
     }
 
